6702143ff24e555c9671aae99d9bb63b02d1e9a1
Hulya <Hulya@DESKTOP-S24GON5>
2024-07-08 22:29:19 +0400

    bug fixed

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,3 @@
-using Education.DataAccess;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,12 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<Context>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 var app = builder.Build();
 
-builder.WebHost.UseUrls("https://*:1000");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
--- API/WeatherForecast.cs
+++ API/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace Education
+namespace API
 {
     public class WeatherForecast
     {
